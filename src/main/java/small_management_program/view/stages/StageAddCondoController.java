2317fe8c8ce0c4759d97582be1e3cd32938122fc
unknown <micheleantonazzi@gmail.com>
2019-03-07 16:04:20 +0100

    update stageAddCondo

--- src/main/java/small_management_program/view/stages/StageAddCondoController.java
+++ src/main/java/small_management_program/view/stages/StageAddCondoController.java
@@ -1,14 +1,27 @@
 package small_management_program.view.stages;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StageAddCondoController implements Initializable {
 
+    @FXML
+    private Button buttonAdd;
+    private Button buttonExit;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    @FXML
+    public void close(ActionEvent event){
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
     }
 }
