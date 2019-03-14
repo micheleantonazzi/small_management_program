91516dee46cf53049df81c910b7c17403d04460f
Michele Antonazzi <micheleantonazzi@gmail.com>
2019-03-14 17:21:44 +0100

    started to develop StageAddCondo

--- src/main/java/small_management_program/view/stages/StageAddCondoController.java
+++ src/main/java/small_management_program/view/stages/StageAddCondoController.java
@@ -4,20 +4,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.stage.Stage;
-
+import javafx.scene.control.ChoiceBox;
+import small_management_program.controller.queries.QueryWithResults;
+import small_management_program.controller.queries.administrator.AdministratorSelectAll;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class StageAddCondoController implements Initializable {
 
     @FXML
-    private Button buttonAdd;
+    private ChoiceBox choiceBoxAdministrators;
+
+    @FXML
+    private Button buttonAddCondo;
+
+    @FXML
     private Button buttonExit;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        QueryWithResults getAdministrator = new AdministratorSelectAll();
     }
 
     public void closeStage(ActionEvent event){}
