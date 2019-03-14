5ddecf79d47f96f620c836604474b1bbece11b6e
Michele Antonazzi <micheleantonazzi@gmail.com>
2019-03-14 16:12:47 +0100

    Added annotation to show alert success in the aspect

---
+++ src/main/java/small_management_program/view/AnnotationShowAlertSuccess.java
@@ -0,0 +1,7 @@
+package small_management_program.view;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AnnotationShowAlertSuccess {}
