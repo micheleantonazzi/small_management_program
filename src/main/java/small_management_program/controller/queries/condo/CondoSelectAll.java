f10ea2bb3bedc98a23cbb7e644cb7774845e196e
Michele Antonazzi <micheleantonazzi@gmail.com>
2019-03-16 17:08:23 +0100

    added method to get choicebox's items

--- src/main/java/small_management_program/controller/queries/condo/CondoSelectAll.java
+++ src/main/java/small_management_program/controller/queries/condo/CondoSelectAll.java
@@ -1,11 +1,16 @@
 package small_management_program.controller.queries.condo;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import small_management_program.controller.DuplicateMap;
 import small_management_program.controller.queries.QueryWithResults;
 import small_management_program.model.database.DatabaseException;
+import small_management_program.view.graphicutilities.ChoiceBoxItemId;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Set;
 
 public class CondoSelectAll extends QueryWithResults {
 
@@ -40,5 +45,17 @@ public class CondoSelectAll extends QueryWithResults {
     @Override
     public DatabaseException getException(){
         return new DatabaseException("Operazione non riuscita", "Attenzione, non \u00E8 stato possibile recuperare i dati dei condomini");
+    }
+
+    public ObservableList<ChoiceBoxItemId> getChoiceBoxItems(){
+        DuplicateMap<Integer, String> condos = this.getResults();
+        ObservableList<ChoiceBoxItemId> condosItems = FXCollections.observableArrayList();
+        Set<Integer> ids = condos.keySet();
+        for(Iterator<Integer> it = ids.iterator(); it.hasNext();){
+            int id = it.next();
+            ChoiceBoxItemId item = new ChoiceBoxItemId(id, id +  " - " + condos.get(id, 3));
+            condosItems.add(item);
+        }
+        return condosItems;
     }
 }
