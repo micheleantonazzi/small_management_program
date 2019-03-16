562d2bc5b222761525783a101a08d1326b5a3755
Michele Antonazzi <micheleantonazzi@gmail.com>
2019-03-16 17:07:25 +0100

    added new queries

---
+++ src/main/java/small_management_program/controller/queries/billing/BillingUpdate.java
@@ -0,0 +1,35 @@
+package small_management_program.controller.queries.billing;
+
+import small_management_program.controller.queries.Query;
+import small_management_program.model.database.DatabaseException;
+
+/**
+ * Modifica una fattura.
+ *
+ * @author Michele Antonazzi
+ */
+
+public class BillingUpdate implements Query {
+
+    private int idCondo;
+    private int year;
+    private double total;
+
+    public BillingUpdate(int idCondo, int year, double total) {
+        this.idCondo = idCondo;
+        this.year = year;
+        this.total = total;
+    }
+
+    @Override
+    public String getQuery() {
+        return "UPDATE billings SET total = " + this.total + " " +
+                "WHERE id_condo = " + idCondo + " AND year = " + this.year;
+    }
+
+    @Override
+    public DatabaseException getException() {
+        return new DatabaseException("Errore database", "Attenzione, impossibile modificare la fatturazione.\n" +
+                "Assicurarsi di non aver assegnato un anno già utilizzato per questo condominio.");
+    }
+}
